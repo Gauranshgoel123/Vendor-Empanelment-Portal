b683bbbc5347d549bfca4697c56c6c8584244757
Gauransh Goel <gauranshgoel04@gmail.com>
2025-07-21 18:14:01 +0530

    setup register login jwt RBAC

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware");
 
-// New: Registration Route
+
+// New: Registration Route //public
+router.get("/", userController.getUsers);
 router.post("/register", userController.registerUser);
 
 // Existing: Fetch users
-router.get("/", userController.getUsers);
+// router.get("/", userController.getUsers);
+
+// Protected route (officials only)
+router.get("/admin-only", verifyToken, authorizeRoles("hod", "gm", "director"), (req, res) => {
+  res.json({ message: "Hello Admin", user: req.user });
+});
 
 module.exports = router;
